c0537cb60b3b666ee720e4353ef9cdd85d26ff53
kentaro@chromebook <kentaro.linux@gmail.com>
2017-04-17 21:58:24 +0900

    Update

--- main.go
+++ main.go
@@ -46,7 +46,8 @@ func main() {
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					replyMessage := "334"
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
 						log.Print(err)
 					}
 				}
