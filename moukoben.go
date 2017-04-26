2d04007777b059e4464d3dbd04a64d65cfd8ec60
kentaro@home <kentaro.linux@gmail.com>
2017-04-26 23:32:07 +0900

    ンゴの判定をYahooAPIを使って柔軟にした

--- moukoben.go
+++ moukoben.go
@@ -1,13 +1,31 @@
 package main
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/hasokon/yahooapi"
+)
 
 // CheckMoukoben is a function that detects any moukoben and return correct reply messages
 func CheckMoukoben(text string) (string, bool) {
+	result, err := yahooapi.MorphologicalAnalysys(text)
+	if err != nil {
+		return "", false
+	}
+
+	isNgo := false
+	wordlist := result.Ma.Wordlist.Wordlist
+	for _, word := range wordlist {
+		if strings.HasPrefix(word.Surface, "ンゴ") {
+			isNgo = true
+			break
+		}
+	}
+
 	switch {
 	case strings.Contains(text, "334"):
 		return "なんでや！阪神関係ないやろ！", true
-	case strings.HasSuffix(text, "ンゴ"):
+	case isNgo:
 		return "はえ〜", true
 	case strings.HasSuffix(text, "や！"):
 		return "(*^◯^*)", true
