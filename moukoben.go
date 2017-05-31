b4efabd96ef81b10f02fdf1908be7f20cc0b9638
Kentaro@vaio-windows <kentaro.linux@gmail.com>
2017-05-31 14:10:04 +0900

    Add Ngo count func

--- moukoben.go
+++ moukoben.go
@@ -6,6 +6,8 @@ import (
 	"github.com/hasokon/yahooapi"
 )
 
+var countNgo = 0
+
 // CheckMoukoben is a function that detects any moukoben and return correct reply messages
 func CheckMoukoben(text string) (string, bool) {
 	result, err := yahooapi.MorphologicalAnalysys(text)
@@ -26,6 +28,11 @@ func CheckMoukoben(text string) (string, bool) {
 	case strings.Contains(text, "334"):
 		return "なんでや！阪神関係ないやろ！", true
 	case isNgo:
+		countNgo++
+		if countNgo >= 15 {
+			countNgo = 0
+			return "ンゴンゴうるさいンゴォ！！\n何回「はえ～」言えばええんや！", true
+		}
 		return "はえ〜", true
 	case strings.HasSuffix(text, "や！"):
 		return "(*^◯^*)", true
